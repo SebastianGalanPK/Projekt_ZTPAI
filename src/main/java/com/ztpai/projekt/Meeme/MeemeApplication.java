df0ff953edb1f10f92841e9aff877716af29abd5
Sebastian Gałan <sebastian.galan@student.pk.edu.pl>
2023-03-17 14:33:38 +0100

    Initial Push

---
+++ src/main/java/com/ztpai/projekt/Meeme/MeemeApplication.java
@@ -0,0 +1,13 @@
+package com.ztpai.projekt.Meeme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeemeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeemeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ztpai/projekt/Meeme/MeemeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ztpai.projekt.Meeme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeemeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
